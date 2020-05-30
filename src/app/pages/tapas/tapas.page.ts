618eec17498a58c6827f5fceca635543db66e830
José Derick Cuesta González <derick@herbecon.es>
2020-05-30 17:09:11 +0200

    Quitar archivos sobrantes y unificar servicios en una misma carpeta

--- src/app/pages/tapas/tapas.page.ts
+++ src/app/pages/tapas/tapas.page.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { TapasService } from '../../../services/tapas.service';
 import { ActivatedRoute } from '@angular/router';
 import { TiposService } from 'src/app/services/tipos.service';
 import { AllergensService } from 'src/app/services/allergens.service';
+import { TapasService } from 'src/app/services/tapas.service';
 
 @Component({
   selector: 'app-tapas',
